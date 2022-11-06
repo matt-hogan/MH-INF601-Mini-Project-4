cc64fa95a3e6b91c6606b8376ed4e700a9084339
matt-hogan <23533809+matt-hogan@users.noreply.github.com>
2022-11-06 15:49:07 -0600

    Add pages for completed and incomplete tasks

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,9 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name = "tasks"
 urlpatterns = [
-    path("", views.TasksView.as_view(), name="index"),
-    # path("completed/", , name="completed"),
+    path("", views.IncompleteTasksView.as_view(), name="index"),
+    path("completed/", views.CompleteTasksView.as_view(), name="completed"),
     # path("create/", , name="create"),
     # path("<int:pk>/update/", , name="update"),
     # path("<int:pk>/dismiss/", , name="dismiss"),
--- tasks/views.py
+++ tasks/views.py
@@ -4,11 +4,22 @@ from django.views import generic
 
 from .models import Task
 
-class TasksView(generic.ListView):
+class IncompleteTasksView(generic.ListView):
     template_name = "tasks/incomplete.html"
     context_object_name = "tasks"
 
     def get_queryset(self):
-        return Task.objects.all()
+        return Task.objects.filter(completed=False).values()
 
 
+class CompleteTasksView(generic.ListView):
+    template_name = "tasks/complete.html"
+    context_object_name = "tasks"
+
+    def get_queryset(self):
+        return Task.objects.filter(completed=True).values()
+
+
+def get_task(id):
+    """ Returns a single task from the database """
+    return Task.objects.get(id=id)
